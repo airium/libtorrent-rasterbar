65c8187b812f62ae0382f6de8aa573860ac0b72e
airium <airium@outlook.com>
2022-01-30 09:06:33 +0000

    Remove torrent up/dl speed smoothing

--- src/stat.cpp
+++ src/stat.cpp
@@ -39,7 +39,8 @@ void stat_channel::second_tick(int tick_interval_ms)
 {
 	std::int64_t sample = std::int64_t(m_counter) * 1000 / tick_interval_ms;
 	TORRENT_ASSERT(sample >= 0);
-	m_5_sec_average = std::int32_t(std::int64_t(m_5_sec_average) * 4 / 5 + sample / 5);
+	// m_5_sec_average = std::int32_t(std::int64_t(m_5_sec_average) * 4 / 5 + sample / 5);
+	m_5_sec_average = std::int32_t(sample);
 	m_counter = 0;
 }
 
